df184b31efd60133939cfe6c579171168b382093
Colin Frank <cfrank@osafo.com>
2021-02-06 20:24:45 -0800

    Exercise 18

---
+++ Section-15/stuff.js
@@ -0,0 +1,28 @@
+// Strings
+
+let favAnimal = "dog";
+
+// String index
+let anaimal = 'Elephant';
+
+animal.length;
+
+// anumal[0] == 'E'
+// anumal[99] == undefined
+
+let firstName = 'River';
+let lastName = 'Phoenix';
+let fullName = firstName + lastName;
+
+typeof fullName;    // "string"
+
+let result = 1 + fullName;
+// 1RiverPhoenix
+typeof result;    // "string"
+
+// String methods vs properties
+let yell = result.toUpperCase();
+
+yell.trim();
+
+
