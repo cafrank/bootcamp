725ef502d28eb0fff2e9e98f35e1d9794a8720ca
Colin Frank <cfrank@osafo.com>
2021-02-06 19:59:20 -0800

    Exercise 16

---
+++ Section-14/Exercise-16/stuff.js
@@ -0,0 +1,25 @@
+// Increment, autoincrement, autodecrement...
+let score = 1;
+score++;
+score += 4;
+
+// Constant 
+const pi = 3.141592654;
+
+
+// OLd ver stype
+let oldScore = 1;   // Obsolete
+
+
+// Changing types sucks.  DOn't do!!!
+
+// Start with 
+
+// Naming convistion
+let n = 4;  // Not descriptive enough
+
+// Snake case.  OK but not best convention
+let foo_bar = 4;
+
+// Camel case.  Good boolean neam
+let isLoggisIn = true;
