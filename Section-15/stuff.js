a049bab4f0cba7bd866dcae0c732f9f66385ec63
Colin Frank <cfrank@osafo.com>
2021-02-06 20:38:56 -0800

    Exercie 19

--- Section-15/stuff.js
+++ Section-15/stuff.js
@@ -26,3 +26,20 @@ let yell = result.toUpperCase();
 yell.trim();
 
 
+// String with atguments
+yell.indexOf('V');
+yell.indexOf('z');      // -1
+
+let nums = '012345789'
+nums.slice(2);
+// '2345789'
+
+nums.slice(-2);
+// '89'
+
+nums.replace('45', 'XX');
+// '0123XX789'
+
+nums.repeat(10);
+// '0123456789012345.....'
+
