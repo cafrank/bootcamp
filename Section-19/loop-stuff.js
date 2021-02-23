609d705958f23f6c76d1319adb4e052ca0c5c90d
Colin Frank <cfrank@osafo.com>
2021-02-22 21:21:38 -0800

    Exercise 31

---
+++ Section-19/loop-stuff.js
@@ -0,0 +1,26 @@
+
+
+for (let i=0; i<6; i++) {
+    console.log(i);
+}
+
+for (let i=0; i<60; i+=5) {
+    console.log(i);
+}
+
+const animals = [ 'lions', 'tigers', 'goats'];
+for (let i=0; i<3; i++) {
+    console.log(i, anamals[i]);
+}
+
+for (let i=0; i<anamals.length; i++) {
+    console.log(i, anamals[i]);
+}
+
+for (let i=0; i<anamals.length; i++) {
+    console.log(i, anamals[i]);
+}
+
+for (let a : anamals) {
+    console.log(a);
+}
