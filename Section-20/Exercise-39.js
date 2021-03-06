bf074a345eab546c518aed680b0698cd0ca03206
Colin Frank <cfrank@osafo.com>
2021-03-05 16:20:01 -0800

    Fixed Exercise-39

---
+++ Section-20/Exercise-39.js
@@ -0,0 +1,12 @@
+// DEFINE YOUR FUNCTION BELOW:
+function isShortsWeather(temp) {
+    // Short answer
+    // return temp >= 75;
+    
+    // Long answer.
+    if (temp >= 75) {
+        return true;
+    } else {
+        return false;
+    }
+}
