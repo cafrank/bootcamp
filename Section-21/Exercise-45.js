8fe823c44d440083af2f5d57c4b8c821148af154
Colin Frank <cfrank@osafo.com>
2021-04-17 19:43:18 -0700

    Exercise 45

---
+++ Section-21/Exercise-45.js
@@ -0,0 +1,9 @@
+const square = {
+    area: function(sideLength) {
+        return sideLength * sideLength;
+    },
+    perimeter: function(sideLength) {
+        return sideLength * 4;
+    }
+
+}
