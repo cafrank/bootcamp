22fe9ff1df68ee5ba13d62fbb6dad55af93e905e
Colin Frank <cfrank@osafo.com>
2021-04-30 08:45:22 -0700

    Exercise-52 update

---
+++ Section-24/Exercise-52.js
@@ -0,0 +1,6 @@
+// Your code goes in here!
+let doneTodos = document.querySelectorAll(".done");
+let checkbox = document.querySelector('input[type="checkbox"]');
+
+// For the checked checkbox
+//let checkbox = document.querySelector('input[type="checkbox"]:checked');
