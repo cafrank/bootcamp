9505bea3d19fa2cb6e47b408455ac551f3c88dec
Colin Frank <cfrank@osafo.com>
2021-04-30 20:15:49 -0700

    Exercise-57

---
+++ Section-24/Exercise-57.js
@@ -0,0 +1,8 @@
+//YOU CODE GOES HERE:
+let items = document.querySelectorAll("ul li");  // Note this is collection. Like array
+
+for (let i=0; i<items.length; i++) {
+    for (let i=0; i<items.length; i++) {
+        items[i].classList.toggle('highlight');
+    }
+}
