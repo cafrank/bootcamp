344ddc029f9467753671002572b94f359f864e57
Colin Frank <cfrank@osafo.com>
2021-04-30 19:04:01 -0700

    Exercise 54

---
+++ Section-24/Exercise-54.js
@@ -0,0 +1,11 @@
+// The url you need: 'https://www.flaticon.com/svg/static/icons/svg/3523/3523063.svg'
+
+// YOUR CODE GOES IN HERE:
+
+// Three waus to get the imang.
+//let image = document.querySelector('img');
+//let image = document.querySelector('#egg');
+let image = document.getElementById("egg");  // I like this when available
+
+image.alt = 'chicken';
+image.src = 'https://www.flaticon.com/svg/static/icons/svg/3523/3523063.svg';
