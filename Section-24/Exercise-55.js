473b9e3a0a4e4231a4752b56d18f061e96589b67
Colin Frank <cfrank@osafo.com>
2021-04-30 19:30:59 -0700

    Exercise-55

---
+++ Section-24/Exercise-55.js
@@ -0,0 +1,9 @@
+let div = document.getElementById("container");
+let image = document.querySelector('img');
+
+image.style.width='150px';
+image.style.borderRadius = '50%';
+
+// Took me 10 minutes, because I didn't read the instructions 
+// Carefully.  Is said allign the Text not the Div !!!
+div.style.textAlign = 'center';  
