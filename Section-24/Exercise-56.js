5e16f9c3c7b84267d1184c9d575e389639295f92
Colin Frank <cfrank@osafo.com>
2021-05-02 10:23:50 -0700

    Got the forEach oneliner working

--- Section-24/Exercise-56.js
+++ Section-24/Exercise-56.js
@@ -9,3 +9,13 @@ let spans = document.querySelectorAll("h1 span");  // Note this is collection. L
 for (let i=0; i<colors.length; i++) {
    spans[i].style.color = colors[i];
 }
+
+// I couldn't let this go.  This works.
+colors.forEach((span,index) => console.log(span, index))
+
+// So the final geek solution is:
+spans.forEach((span,index) => span.style.color = colors[index])
+
+// The jackass programmer one line solution.
+// Remember KISS.  Keep It Simple Stupid!
+document.querySelectorAll("h1 span").forEach((s,i) => s.style.color = colors[i]);
